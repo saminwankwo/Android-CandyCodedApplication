01709752626426851fc6429f688c78c50d282cb3
Nwankwo Samuel <nwankwosami@gmail.com>
2020-10-05 02:21:42 +0100

    Finished Execrise three: start phone activity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -90,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
         Intent infoIntent = new Intent(this, InfoActivity.class);
         startActivity(infoIntent);
+
         return super.onOptionsItemSelected(item);
     }
 
